864d5a63a9570ce095d2c313403d9769d087bba2
Leonardo <leodinsouza@yahoo.com.br>
2019-01-19 16:21:08 -0200

    relationship

--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use App\User;
+use App\Models\Like;
+use App\Models\Question;
 use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
-    //
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
 }
