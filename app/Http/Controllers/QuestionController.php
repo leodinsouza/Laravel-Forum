78752eacf2de2f71e6631aa04261f4f25327dbde
Leonardo <leodinsouza@yahoo.com.br>
2019-01-20 15:52:13 -0200

    QuestionResource

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Question;
 use Illuminate\Http\Request;
+use \Illuminate\Http\Response;
+use App\Http\Resources\QuestionResource;
 
 class QuestionController extends Controller
 {
@@ -14,18 +16,9 @@ class QuestionController extends Controller
      */
     public function index()
     {
-        //
+        return QuestionResource::collection(Question::latest()->get());
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -35,7 +28,8 @@ class QuestionController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        Question::create($request->all());
+        return response('Criado', Response::HTTP_CREATED);
     }
 
     /**
@@ -46,7 +40,8 @@ class QuestionController extends Controller
      */
     public function show(Question $question)
     {
-        //
+        //model binding
+        return new QuestionResource($question);
     }
 
     /**
@@ -80,6 +75,7 @@ class QuestionController extends Controller
      */
     public function destroy(Question $question)
     {
-        //
+        $question->delete();
+        return response('Removido', 201);
     }
 }
